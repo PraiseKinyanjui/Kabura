38cf3f7a5279a453b3719a80f93b14b0d28e6c99
Praise kinyanjui <157103335+PraiseKinyanjui@users.noreply.github.com>
2026-03-24 18:40:37 +0300

    Create tailwind.config.js for custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        burgundy: '#800020',
+        'burgundy-dark': '#4b0000',
+        cream: '#fffdd0',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
